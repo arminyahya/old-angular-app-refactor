c26b5c7548d50fca1e47d151233a8f65c86aee4e
arminyahya <arminyahyaa@gmail.com>
2026-02-21 20:27:20 +0330

    refactor: simplify two-way data binding in task panel component and remove unused outputs

--- modern-angular/src/app/components/task-panel/task-panel.component.ts
+++ modern-angular/src/app/components/task-panel/task-panel.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, model } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { Task } from '../../models/task.model';
@@ -14,33 +14,14 @@ import { TaskStatsComponent } from '../task-stats/task-stats.component';
 export class TaskPanelComponent {
   @Input() tasks: Task[] = [];
   @Input() filteredTasks: Task[] = [];
-  @Input() newTaskTitle = '';
-  @Input() newTaskPriority = 'medium';
-  @Input() filterState = 'all';
-  @Input() searchText = '';
+  newTaskTitle = model('');
+  newTaskPriority = model('medium');
+  filterState = model('all');
+  searchText = model('');
   @Input() openCount = 0;
   @Input() doneCount = 0;
-
-  @Output() newTaskTitleChange = new EventEmitter<string>();
-  @Output() newTaskPriorityChange = new EventEmitter<string>();
-  @Output() filterStateChange = new EventEmitter<string>();
-  @Output() searchTextChange = new EventEmitter<string>();
 
   @Output() add = new EventEmitter<void>();
   @Output() remove = new EventEmitter<Task>();
   @Output() toggleDone = new EventEmitter<Task>();
-  @Output() applyFilter = new EventEmitter<void>();
-
-  private searchDebounceTimer: ReturnType<typeof setTimeout> | null = null;
-
-  onSearchTextChanged(value: string): void {
-    this.searchTextChange.emit(value);
-    if (this.searchDebounceTimer) {
-      clearTimeout(this.searchDebounceTimer);
-    }
-
-    this.searchDebounceTimer = setTimeout(() => {
-      this.applyFilter.emit();
-    }, 150);
-  }
 }
